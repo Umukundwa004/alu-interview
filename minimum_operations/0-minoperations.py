336e923c802a55dde09a72a60e6c21f453a3443e
Umukundwa004 <v.umukundwa@alustudent.com>
2025-06-19 13:24:11 +0200

    done

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""the `n` returns minoperations"""
+
+
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
